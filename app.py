2d463855f764c2af91d2dc0e807ff9a0801fd8e8
Cserjési Kristóf <cserjesik@gmail.com>
2020-08-13 14:03:11 +0200

    Token kezelő függvények és ellenőrzések létrehozva.

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 import uuid
+import jwt
+from functools import wraps
 
 app = Flask(__name__)
 
@@ -15,10 +17,10 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     public_id = db.Column(db.String(50), unique = True)
-    name =  db.Column(db.String(50))
+    name =  db.Column(db.String(50), unique = True)
     password =  db.Column(db.String(80))
     admin = db.Column(db.Boolean)
-    date_created = db.Column(db.DateTime, default=datetime.now)
+    date_created = db.Column(db.DateTime, default=datetime.datetime.now)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -26,9 +28,33 @@ class Todo(db.Model):
     complete = db.Column(db.Boolean)
     user_id = db.Column(db.Integer)
 
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+        
+        if not token:
+            return jsonify({'message':'hiányzó token'})
+        
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            current_user = User.query.filter_by(public_id=data['public_id']).first()
+        except:
+            return jsonify({'message':'invalid token'}), 401
+        return f(current_user, *args, **kwargs)
+
+    return decorated
+
 
 @app.route('/user', methods=['GET'])
-def get_all_users():
+@token_required
+def get_all_users(current_user):
+
+    if not current_user.admin:
+        return jsonify({'message': 'A funkció nem érhető el!'})
+
     users = User.query.all()
     output = []
     for user in users:
@@ -41,7 +67,11 @@ def get_all_users():
     return jsonify({'users': output})
 
 @app.route('/user/<public_id>', methods=['GET'])
-def get_one_user(public_id):
+@token_required
+def get_one_user(current_user, public_id):
+    if not current_user.admin:
+        return jsonify({'message': 'A funkció nem érhető el!'})
+    
     user = User.query.filter_by(public_id=public_id).first()
     if not user:
         return jsonify({'message':'nem létező felhasználó'})
@@ -53,7 +83,10 @@ def get_one_user(public_id):
     return jsonify({'user': user_data})
 
 @app.route('/user', methods=['POST'])
-def create_user():
+@token_required
+def create_user(current_user):
+    if not current_user.admin:
+        return jsonify({'message': 'A funkció nem érhető el!'})
     data = request.get_json()
     hashed_password = generate_password_hash(data['password'], method='sha256')
     new_user = User(public_id=str(uuid.uuid4()), name=data['name'], password=hashed_password, admin=False)
@@ -62,7 +95,11 @@ def create_user():
     return jsonify({'message': 'új felhasználó sikeresen létrehozva'})
 
 @app.route('/user/<public_id>', methods=['PUT'])
-def promote_user(public_id):
+@token_required
+def promote_user(current_user, public_id):
+    if not current_user.admin:
+        return jsonify({'message': 'A funkció nem érhető el!'})
+
     user = User.query.filter_by(public_id=public_id).first()
     if not user:
         return jsonify({'message':'nem létező felhasználó'})
@@ -72,7 +109,10 @@ def promote_user(public_id):
 
 
 @app.route('/user/<public_id>', methods=['DELETE'])
-def delete_user(public_id):
+@token_required
+def delete_user(current_user, public_id):
+    if not current_user.admin:
+        return jsonify({'message': 'A funkció nem érhető el!'})
     user = User.query.filter_by(public_id=public_id).first()
     if not user:
         return jsonify({'message':'nem létező felhasználó'})
@@ -80,6 +120,23 @@ def delete_user(public_id):
     db.session.commit()
     return jsonify({'message':'a felhasználó sikeresen törölve'})
 
+@app.route('/login', methods=['POST'])
+def login():
+    auth = request.authorization
+
+    if not auth or not auth.username or not auth.password:
+        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic-realm="Login required!"'})
+
+    user = User.query.filter_by(name=auth.username).first()
+    if not user:
+        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic-realm="Login required!"'})
+
+    if check_password_hash(user.password, auth.password):
+        token = jwt.encode({'public_id': user.public_id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+        return jsonify({'token':token.decode('UTF-8')})
+
+    return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic-realm="Login required!"'})
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
